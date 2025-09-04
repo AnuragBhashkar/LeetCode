41025b4a00c609f483a4c57052855428996bd3b6
Anurag Bhashkar <anuragbhashkarofficial@gmail.com>
2025-09-05 02:57:35 +0530

    Time: 1 ms (19.26%) | Memory: 46.5 MB (9.01%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -4,10 +4,14 @@ class Solution {
         int last=-1;
         for(int i=0;i<nums.length;i++){
             if(nums[i]==target){
-                if(first==-1){
-                    first=i;
-                }
-                last=i;
+                first=i;
+                break;
+            }
+        }
+        for(int j=nums.length-1;j>=0;j--){
+            if(nums[j]==target){
+                last=j;
+                break;
             }
         }
         return new int[]{first,last};
