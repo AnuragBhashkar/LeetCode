1265f2245cd97e9701511d05151728d5d8877419
Anurag Bhashkar <anuragbhashkarofficial@gmail.com>
2025-11-17 20:14:37 +0530

    Time: 2 ms (24.04%) | Memory: 45.1 MB (19.43%) - LeetSync

--- 443-string-compression/string-compression.java
+++ 443-string-compression/string-compression.java
@@ -1,18 +1,18 @@
 class Solution {
     public int compress(char[] ch) {
-        String ans="";
+        StringBuilder ans=new StringBuilder("");
         int i=0;
         int j=0;
         while(j<ch.length){
             if(ch[i]==ch[j]) j++;
             else{
-                ans+=ch[i];
-                if(j-i>1) ans+=(j-i);
+                ans.append(ch[i]);
+                if(j-i>1) ans.append(j-i);
                 i=j;
             }
         }
-        ans+=ch[i];
-        if(j-i>1) ans+=(j-i);
+        ans.append(ch[i]);
+        if(j-i>1) ans.append(j-i);
         for(int k=0;k<ans.length();k++){
             ch[k]=ans.charAt(k);
         }
