a2bec8867caaae681faedfd25eda85e216ea8bfd
Anurag Bhashkar <anuragbhashkarofficial@gmail.com>
2025-12-01 17:35:58 +0530

    Time: 11 ms (63.45%) | Memory: 50.8 MB (39.42%) - LeetSync

---
+++ 1408-find-the-smallest-divisor-given-a-threshold/find-the-smallest-divisor-given-a-threshold.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int smallestDivisor(int[] nums, int threshold) {
+        int max=0;
+        for(int i=0;i<nums.length;i++){
+            max=Math.max(max,nums[i]);
+        }
+        int d=-1;
+        int low=1,high=max;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            int sum=0;
+            for(int i=0;i<nums.length;i++){
+                if(nums[i]%mid==0){
+                    sum+=nums[i]/mid;
+                }
+                else{
+                    sum+=(nums[i]/mid)+1;
+                }
+                if(sum>threshold) break;
+            }
+            if(sum<=threshold){
+                d=mid;
+                high=mid-1;
+            }
+            else low=mid+1;
+        }
+        return d;
+    }
+}
