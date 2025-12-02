cc0ab1d0541c5d49ca0a95954a242cd33c28d797
Anurag Bhashkar <anuragbhashkarofficial@gmail.com>
2025-12-02 18:48:24 +0530

    Time: 11 ms (74.44%) | Memory: 50.4 MB (5.10%) - LeetSync

--- 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.java
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.java
@@ -1,22 +1,6 @@
 class Solution {
-    public static boolean isPossible(int[] arr,int days,int mid){
-        int load=0;
-        int D=1;
-        for(int i=0;i<arr.length;i++){
-            if(load+arr[i]<=mid){
-                load+=arr[i];
-            }
-            else{
-                load=arr[i];
-                D++;
-            } 
-        }
-        if(D>days) return false;
-        else return true;
-    }
     public int shipWithinDays(int[] weights, int days) {
-        int max=0;
-        int sum=0;
+        int max=0,sum=0;
         for(int i=0;i<weights.length;i++){
             max=Math.max(max,weights[i]);
             sum+=weights[i];
@@ -24,12 +8,27 @@ class Solution {
         int low=max,high=sum,minC=sum;
         while(low<=high){
             int mid=low+(high-low)/2;
-            if(isPossible(weights,days,mid)==true){
-                minC=mid;
+            if(isPossible(weights,mid,days)==true){
                 high=mid-1;
+                minC=mid;
             }
             else low=mid+1;
         }
         return minC;
+    }
+
+    public static boolean isPossible(int[] arr,int minC,int days){
+        int load=0,D=1;
+        for(int i=0;i<arr.length;i++){
+            if(load+arr[i]<=minC){
+                load+=arr[i];
+            }
+            else{
+                load=arr[i];
+                D++;
+            }
+        }
+        if(D>days) return false;
+        else return true;
     }
 }
