a971e26c375cde15c13de104f4de05972d266ac4
Anurag Bhashkar <anuragbhashkarofficial@gmail.com>
2025-12-09 04:21:03 +0530

    Time: 2 ms (58.35%) | Memory: 44.7 MB (34.21%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.java
+++ 22-generate-parentheses/generate-parentheses.java
@@ -1,10 +1,5 @@
 class Solution {
     static ArrayList<String> ans;
-    public List<String> generateParenthesis(int n) {
-        ans=new ArrayList<>();
-        generate(0,0,"",n);
-        return ans;
-    }
     public void generate(int open,int close,String s,int n){
         if(s.length()==2*n){
             ans.add(s);
@@ -12,5 +7,10 @@ class Solution {
         }
         if(open<n) generate(open+1,close,s+'(',n);
         if(close<open) generate(open,close+1,s+')',n);
+    }
+    public List<String> generateParenthesis(int n) {
+        ans=new ArrayList<>();
+        generate(0,0,"",n);
+        return ans;
     }
 }
