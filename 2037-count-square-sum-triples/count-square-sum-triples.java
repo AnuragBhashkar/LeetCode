8b38292f829c7a8039fe4917147aba9357d98d0f
Anurag Bhashkar <anuragbhashkarofficial@gmail.com>
2025-12-09 01:31:39 +0530

    Time: 233 ms (15.98%) | Memory: 42.2 MB (38.59%) - LeetSync

--- 2037-count-square-sum-triples/count-square-sum-triples.java
+++ 2037-count-square-sum-triples/count-square-sum-triples.java
@@ -4,9 +4,9 @@ class Solution {
         for(int a=1;a<=n;a++){
             for(int b=1;b<=n;b++){
                 for(int c=1;c<=n;c++){
-                    int sum=a*a+b*b;
-                    int c2=c*c;
-                    if(c2==sum) count++;
+                    // int sum=a*a+b*b;
+                    // int c2=c*c;
+                    if((a*a+b*b)==c*c) count++;
                 }
             }
         }
