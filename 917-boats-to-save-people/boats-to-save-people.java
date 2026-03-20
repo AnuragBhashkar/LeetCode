5aa4a8606a4fc52d2d17908469a4900f10c7dc86
Anurag Bhashkar <anuragbhashkarofficial@gmail.com>
2026-03-20 17:34:52 +0530

    Time: 20 ms (83.08%) | Memory: 56.6 MB (32.73%) - LeetSync

---
+++ 917-boats-to-save-people/boats-to-save-people.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+        Arrays.sort(people);
+        int count=0;
+        int lo=0,hi=people.length-1;
+        while(lo<=hi){
+            if(people[lo]+people[hi]<=limit){
+                lo++;
+            }
+            hi--;
+            count++;
+        }
+        return count;
+    }
+}
