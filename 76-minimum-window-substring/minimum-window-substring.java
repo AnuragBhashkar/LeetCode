35fa3d5c58cbe135ce7707c72019feb078294562
Anurag Bhashkar <anuragbhashkarofficial@gmail.com>
2026-04-16 00:27:20 +0530

    Time: 17 ms (51.64%) | Memory: 47.1 MB (51.02%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.java
+++ 76-minimum-window-substring/minimum-window-substring.java
@@ -1,36 +1,40 @@
 class Solution {
     public String minWindow(String s, String t) {
-        if(t.length()>s.length()) return "";
+        if (t.length() > s.length()) return "";
 
-        Map<Character,Integer> map=new HashMap<>();
-        for(char ch:t.toCharArray()){
-            map.put(ch,map.getOrDefault(ch,0)+1);
+        Map<Character, Integer> map = new HashMap<>();
+        for (char ch : t.toCharArray()) {
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
         }
-        
-        int i=0,j=0;
-        int minWindowSize=Integer.MAX_VALUE;
-        int requiredCount=t.length();
-        int start_i=0;
 
-        while(j<s.length()){
+        int i = 0, j = 0;
+        int requiredCount = t.length();
+        int minLen = Integer.MAX_VALUE, start = 0;
+
+        while (j < s.length()) {
             char ch = s.charAt(j);
-            if (map.containsKey(ch) && map.get(ch) > 0) requiredCount--;
-            map.put(ch, map.getOrDefault(ch, 0) - 1);
+
+            if (map.containsKey(ch)) {
+                if (map.get(ch) > 0) requiredCount--;
+                map.put(ch, map.get(ch) - 1);
+            }
+
             while (requiredCount == 0) {
-                int currWindowSize = (j-i+1);
-                if (minWindowSize > currWindowSize) {
-                    minWindowSize = currWindowSize;
-                    start_i = i;
+                if (j - i + 1 < minLen) {
+                    minLen = j - i + 1;
+                    start = i;
                 }
-                char startChar = s.charAt(i);
-                map.put(startChar, map.getOrDefault(startChar, 0) + 1);
-                if (map.containsKey(startChar) && map.get(startChar) > 0) {
-                    requiredCount++;
+
+                char left = s.charAt(i);
+                if (map.containsKey(left)) {
+                    map.put(left, map.get(left) + 1);
+                    if (map.get(left) > 0) requiredCount++;
                 }
                 i++;
             }
             j++;
         }
-        return minWindowSize==Integer.MAX_VALUE? "" :s.substring(start_i,start_i+minWindowSize);
+
+        return minLen == Integer.MAX_VALUE ? "" : s.substring(start, start + minLen);
     }
 }
