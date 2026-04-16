006ef16d5f4766d53e04cfd8472c6635148e857d
Anurag Bhashkar <anuragbhashkarofficial@gmail.com>
2026-04-16 16:33:51 +0530

    Time: 16 ms (59.75%) | Memory: 46.5 MB (61.65%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.java
+++ 76-minimum-window-substring/minimum-window-substring.java
@@ -1,32 +1,28 @@
 class Solution {
     public String minWindow(String s, String t) {
         if(t.length()>s.length()) return "";
-
         Map<Character,Integer> map=new HashMap<>();
-        for(char ch:t.toCharArray()){
-            map.put(ch,map.getOrDefault(ch,0)+1);
-        }
+        for(char ch:t.toCharArray()) map.put(ch,map.getOrDefault(ch,0)+1);
 
-        int i=0,j=0;
+        int i=0,j=0,start=0;
+        int count=t.length();
         int minLen=Integer.MAX_VALUE;
-        int start=i;
-        int requiredCount=t.length();
         while(j<s.length()){
             char ch=s.charAt(j);
             if(map.containsKey(ch)){
-                if(map.get(ch)>0) requiredCount--;
-                map.put(ch,map.getOrDefault(ch,0)-1);
+                if(map.get(ch)>0) count--;
+                map.put(ch,map.get(ch)-1);
             }
-
-            while(requiredCount==0){  //this while loop is for shrinking:
-                if((j-i+1)<minLen){
+            while(count==0){
+                if(j-i+1<minLen){
                     minLen=(j-i+1);
                     start=i;
                 }
+
                 char left=s.charAt(i);
                 if(map.containsKey(left)){
-                    map.put(left,map.getOrDefault(left,0)+1);
-                    if(map.get(left)>0) requiredCount++;
+                    map.put(left,map.get(left)+1);
+                    if(map.get(left)>0) count++;
                 }
                 i++;
             }
