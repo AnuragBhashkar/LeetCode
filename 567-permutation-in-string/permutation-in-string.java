bd07a2be3fe822cc7446ab732f85888ce222f184
Anurag Bhashkar <anuragbhashkarofficial@gmail.com>
2026-04-28 16:34:21 +0530

    Time: 904 ms (5.09%) | Memory: 46.9 MB (18.67%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.java
+++ 567-permutation-in-string/permutation-in-string.java
@@ -2,10 +2,9 @@ class Solution {
     public boolean checkInclusion(String s1, String s2) {
         if(s1.length()>s2.length()) return false;
         Map<Character,Integer> map1=new HashMap<>();
-        for(int i=0;i<s1.length();i++){
-            map1.put(s1.charAt(i),map1.getOrDefault(s1.charAt(i),0)+1);
+        for(char ch:s1.toCharArray()){
+            map1.put(ch,map1.getOrDefault(ch,0)+1);
         }
-
         for(int i=0;i<=s2.length()-s1.length();i++){
             Map<Character,Integer> map2=new HashMap<>();
             for(int j=i;j<i+s1.length();j++){
