6f53e00432d13edf1359289f9bf8695b983d3da3
Anurag Bhashkar <anuragbhashkarofficial@gmail.com>
2026-05-11 19:51:02 +0530

    Time: 9 ms (98.59%) | Memory: 59.4 MB (55.21%) - LeetSync

--- 148-sort-list/sort-list.java
+++ 148-sort-list/sort-list.java
@@ -10,12 +10,10 @@
  */
 class Solution {
     public ListNode sortList(ListNode head) {
-        if(head == null || head.next == null){
-            return head;
-        }
+        if(head == null || head.next == null) return head;
         ListNode slow = head;
-        ListNode fast = head;
-        while(fast.next!=null && fast.next.next!=null){
+        ListNode fast = head.next;
+        while(fast != null && fast.next != null){
             slow = slow.next;
             fast = fast.next.next;
         }
@@ -25,30 +23,25 @@ class Solution {
         ListNode right = sortList(mid);
         return merge(left, right);
     }
-    public ListNode merge(ListNode list1,ListNode list2){
-        ListNode dummy=new ListNode(0);
-        ListNode curr=dummy;
-        while(list1!=null && list2!=null){
-            if(list1.val<=list2.val){
-                curr.next=list1;
-                list1=list1.next;
+    public ListNode merge(ListNode l1, ListNode l2){
+        ListNode dummy = new ListNode(-1);
+        ListNode temp = dummy;
+        while(l1 != null && l2 != null){
+            if(l1.val <= l2.val){
+                temp.next = l1;
+                l1 = l1.next;
             }
             else{
-                curr.next=list2;
-                list2=list2.next;
+                temp.next = l2;
+                l2 = l2.next;
             }
-            curr=curr.next;
+            temp = temp.next;
         }
-
-        while(list1!=null){
-            curr.next=list1;
-            list1=list1.next;
-            curr=curr.next;
+        if(l1 != null){
+            temp.next = l1;
         }
-        while(list2!=null){
-            curr.next=list2;
-            list2=list2.next;
-            curr=curr.next;
+        if(l2 != null){
+            temp.next = l2;
         }
         return dummy.next;
     }
