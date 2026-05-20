c16807ef72dd40ba4638c4c5ea3aadf6e1c9cfc5
Anurag Bhashkar <anuragbhashkarofficial@gmail.com>
2026-05-20 18:19:50 +0530

    Time: 2 ms (88.79%) | Memory: 49.3 MB (52.53%) - LeetSync

--- 143-reorder-list/reorder-list.java
+++ 143-reorder-list/reorder-list.java
@@ -10,36 +10,35 @@
  */
 class Solution {
     public void reorderList(ListNode head) {
-        // 1).Finding middle of the linked list:
-        ListNode slow=head,fast=head;
-        while(fast!=null && fast.next!=null){
-            slow=slow.next;
-            fast=fast.next.next;
+        ListNode slow = head, fast = head;
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
         }
-        ListNode head2=slow.next;
-        slow.next=null;
+        ListNode head2 = slow.next;
+        slow.next = null;
 
-        // 2).Reversing the second half of the linked list:
-        ListNode next=null, curr=head2, prev=null;
-        while(curr!=null){
-            next=curr.next;
-            curr.next=prev;
-            prev=curr;
-            curr=next;
+        ListNode prev = null, next = null, curr = head2;
+        while (curr != null) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
         }
-        
-        // 3).Joining both halves of the linked list:
-        ListNode first=head;
-        ListNode second=prev;
-        while(second!=null){
-            ListNode firstNext=first.next;
-            ListNode secondNext=second.next;
 
-            first.next=second;
-            second.next=firstNext;
+        ListNode dummy=new ListNode(0);
+        ListNode temp=dummy;
+        head2=prev;
+        while(head!=null && head2!=null){
+            temp.next=head;
+            head=head.next;
+            temp=temp.next;
 
-            first=firstNext;
-            second=secondNext;
+            temp.next=head2;
+            head2=head2.next;
+            temp=temp.next;
         }
+        if(head!=null) temp.next=head;
+        if(head2!=null) temp.next=head2;
     }
 }
