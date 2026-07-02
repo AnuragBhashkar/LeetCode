ac94c561e5091b569c1db6dd9618df1b922bf43c
Anurag Bhashkar <anuragbhashkarofficial@gmail.com>
2026-07-02 17:56:22 +0530

    Time: 1305 ms (5.06%) | Memory: 47.7 MB (32.24%) - LeetSync

--- 135-candy/candy.java
+++ 135-candy/candy.java
@@ -3,21 +3,23 @@ class Solution {
         int n=ratings.length;
         int[] candy=new int[n];
         Arrays.fill(candy,1);
+        boolean changed = true;
 
-        //left to right:
-        for(int i=1;i<n;i++){
-            if(ratings[i]>ratings[i-1]){
-                candy[i]=candy[i-1]+1;
+        while (changed) {
+            changed = false;
+            for (int i = 0; i < n; i++) {
+                // Check left neighbor
+                if (i > 0 && ratings[i] > ratings[i - 1] && candy[i] <= candy[i - 1]) {
+                    candy[i] = candy[i - 1] + 1;
+                    changed = true;
+                }
+                // Check right neighbor
+                if (i < n - 1 && ratings[i] > ratings[i + 1] && candy[i] <= candy[i + 1]) {
+                    candy[i] = candy[i + 1] + 1;
+                    changed = true;
+                }
             }
         }
-        
-        //right to left:
-        for(int i=n-2;i>=0;i--){
-            if(ratings[i]>ratings[i+1]){
-                candy[i]=Math.max(candy[i+1]+1,candy[i]);
-            }
-        }
-
         int sum=0;
         for(int c:candy) sum+=c;
         return sum;
