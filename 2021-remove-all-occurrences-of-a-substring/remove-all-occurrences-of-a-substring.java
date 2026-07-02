828fe073899e910cd5bbddd93658eed62d4973f0
Anurag Bhashkar <anuragbhashkarofficial@gmail.com>
2026-07-02 16:59:06 +0530

    Time: 5 ms (49.78%) | Memory: 45.7 MB (47.44%) - LeetSync

--- 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.java
+++ 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.java
@@ -1,8 +1,14 @@
 class Solution {
     public String removeOccurrences(String s, String part) {
-        StringBuilder sb=new StringBuilder(s);
-        int idx;
-        while((idx=sb.indexOf(part))!=-1) sb.delete(idx,idx+part.length());
+        StringBuilder sb=new StringBuilder();
+        for(char ch:s.toCharArray()){
+            sb.append(ch);
+            if(sb.length()>=part.length()){
+                if(sb.substring(sb.length()-part.length()).equals(part)){
+                    sb.delete(sb.length()-part.length(),sb.length());
+                }
+            }
+        }
         return sb.toString();
     }
 }
