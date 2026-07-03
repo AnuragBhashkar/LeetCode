c907c060be3d80e65b3fe7ab9e00d349d2542582
Anurag Bhashkar <anuragbhashkarofficial@gmail.com>
2026-07-03 18:29:01 +0530

    Time: 42 ms (5.03%) | Memory: 47.3 MB (9.74%) - LeetSync

--- 224-basic-calculator/basic-calculator.java
+++ 224-basic-calculator/basic-calculator.java
@@ -1,40 +1,53 @@
 class Solution {
     public int calculate(String s) {
-        Stack<Integer> st=new Stack<>();
-        int num=0;
-        int result=0;
-        int sign=1;
-        for(int i=0;i<s.length();i++){
+        Stack<Integer> nums=new Stack<>();
+        Stack<Character> ops=new Stack<>();
+        int i=0;
+        while(i<s.length()){
             char ch=s.charAt(i);
+            if(ch==' '){
+                i++;
+                continue;
+            }
             if(Character.isDigit(ch)){
-                num=(num*10)+(ch-'0'); //number bana rhe apn (for 2-diginumbers, e.g "10"->10)
+                int num=0;
+                while(i<s.length() && Character.isDigit(s.charAt(i))){
+                    num=(num*10)+(s.charAt(i)-'0');
+                    i++;
+                }
+                nums.push(num);
+                continue;
             }
-            else if(ch=='+'){
-                result+=(sign*num);
-                num=0;
-                sign=1;
+            if(ch=='('){
+                ops.push(ch);
             }
-            else if(ch=='-'){
-                result+=(sign*num);
-                num=0;
-                sign=-1;
-            }
-            else if(ch=='('){
-                result+=(sign*num);
-                st.push(result);
-                st.push(sign);
-                result=0;
-                sign=1;
-                num=0;
+            else if(ch=='+' || ch=='-'){
+                int j = i - 1;
+                while (j >= 0 && s.charAt(j) == ' ') j--;
+                if (j < 0 || s.charAt(j) == '(') {
+                    nums.push(0);
+                }
+                while (!ops.isEmpty() && ops.peek() != '(') {
+                    evaluate(nums, ops);
+                }
+                ops.push(ch);
             }
             else if(ch==')'){
-                result+=(sign*num);
-                result=result*st.pop()+st.pop();
-                num=0;
-                sign=1;
+                while(!ops.isEmpty() && ops.peek()!='('){
+                    evaluate(nums,ops);
+                }
+                ops.pop();
             }
+            i++;
         }
-        result+=(sign*num);
-        return result;
+        while(!ops.isEmpty()) evaluate(nums,ops);
+        return nums.pop();
+    }
+    private void evaluate(Stack<Integer> nums,Stack<Character> ops){
+        int b=nums.pop();
+        int a=nums.pop();
+        char op=ops.pop();
+        if(op=='+') nums.push(a+b);
+        else nums.push(a-b);
     }
 }
