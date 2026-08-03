d36d6636eb5c2dbcd14a88aaa5a42867f1d1a527
Anurag Bhashkar <anuragbhashkarofficial@gmail.com>
2026-08-03 21:29:58 +0530

    Time: 18 ms (81.93%) | Memory: 47.3 MB (58.75%) - LeetSync

--- 1320-remove-all-adjacent-duplicates-in-string-ii/remove-all-adjacent-duplicates-in-string-ii.java
+++ 1320-remove-all-adjacent-duplicates-in-string-ii/remove-all-adjacent-duplicates-in-string-ii.java
@@ -1,23 +1,23 @@
 class Solution {
     public String removeDuplicates(String s, int k) {
         Deque<int[]> st=new ArrayDeque<>();
-        for(char ch:s.toCharArray()){
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
             if(!st.isEmpty() && st.peek()[0]==ch){
                 st.peek()[1]++;
                 if(st.peek()[1]==k) st.pop();
             }
-            else{
-                st.push(new int[]{ch,1});
-            }
+            else st.push(new int[]{ch,1});
         }
-
         StringBuilder sb=new StringBuilder();
         while(!st.isEmpty()){
             int[] pair=st.pop();
-            char ch=(char) pair[0];
+            char ch=(char)pair[0];
             int repeat=pair[1];
             for(int i=0;i<repeat;i++) sb.append(ch);
         }
+
         return sb.reverse().toString();
+
     }
 }
