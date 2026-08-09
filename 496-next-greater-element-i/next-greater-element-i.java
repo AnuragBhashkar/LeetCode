cd4f8c56b1d71b073c74d9c8fb4af347df47e1a4
Anurag Bhashkar <anuragbhashkarofficial@gmail.com>
2026-08-09 17:15:16 +0530

    Time: 7 ms (5.05%) | Memory: 45.1 MB (62.16%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.java
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -1,19 +1,24 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        Stack<Integer> st=new Stack<>();
+        Deque<Integer> st=new ArrayDeque<>();
         int[] ans=new int[nums2.length];
         for(int i=nums2.length-1;i>=0;i--){
             while(!st.isEmpty() && st.peek()<nums2[i]) st.pop();
             if(st.isEmpty()) ans[i]=-1;
-            else ans[i]=st.peek();
+            else{
+                ans[i]=st.peek();
+            }
             st.push(nums2[i]);
         }
-        int[] res=new int[nums1.length];
+
+        int[] result=new int[nums1.length];
         for(int i=0;i<nums1.length;i++){
             for(int j=0;j<nums2.length;j++){
-                if(nums1[i]==nums2[j]) res[i]=ans[j];
+                if(nums1[i]==nums2[j]){
+                    result[i]=ans[j];
+                }
             }
         }
-        return res;
+        return result;
     }
 }
