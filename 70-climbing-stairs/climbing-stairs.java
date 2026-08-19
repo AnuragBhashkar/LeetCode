52960a00c1ff26744ad9f23909c25740c98a93d4
Anurag Bhashkar <anuragbhashkarofficial@gmail.com>
2026-08-20 01:37:30 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (36.55%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,12 +1,16 @@
 class Solution {
+    // Memoization Approach:- 
+    //--------------------------//
+    int[] dp;
     public int climbStairs(int n) {
-        if(n<=1) return 1;
-        int[] dp=new int[n+1];
-        dp[0]=1;
-        dp[1]=1;
-        for(int i=2;i<=n;i++){
-            dp[i]=dp[i-1]+dp[i-2];
-        }
+        dp=new int[n+1];
+        Arrays.fill(dp,-1);
+        return solve(n);
+    }
+    int solve(int n){
+        if(n==0 || n==1) return 1;
+        if(dp[n]!=-1) return dp[n];
+        dp[n]=solve(n-1)+solve(n-2);
         return dp[n];
     }
 }
