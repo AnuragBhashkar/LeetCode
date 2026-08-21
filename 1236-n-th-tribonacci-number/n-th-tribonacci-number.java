2a39454ec0e087eb7fd0af36a88dc417f5f08a93
Anurag Bhashkar <anuragbhashkarofficial@gmail.com>
2026-08-21 22:46:33 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (71.89%) - LeetSync

---
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.java
@@ -0,0 +1,20 @@
+class Solution {
+    int[] dp;
+    public int tribonacci(int n) {
+        if(n==0) return 0;
+        if(n==1 || n==2) return 1;
+        dp=new int[n+1];
+        Arrays.fill(dp,-1);
+        dp[0]=0;
+        dp[1]=1;
+        dp[2]=2;
+        return solve(n);
+    }
+    int solve(int n){
+        if(n==0) return 0;
+        if(n==1 || n==2) return 1;
+        if(dp[n]!=-1) return dp[n];
+        dp[n]=solve(n-1)+solve(n-2)+solve(n-3);
+        return dp[n];
+    }
+}
