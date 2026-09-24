ff44e08470d7437e39d4f4fa50aa09e4d1c77f0e
Anurag Bhashkar <anuragbhashkarofficial@gmail.com>
2026-09-24 19:58:42 +0530

    Time: 2 ms (97.64%) | Memory: 72.6 MB (30.92%) - LeetSync

---
+++ 890-lemonade-change/lemonade-change.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean lemonadeChange(int[] bills) {
+        int five=0,ten=0;
+        for(int x:bills){
+            if(x==5) five++;
+            else if(x==10){
+                if(five>0){
+                    five--;
+                    ten++;
+                }
+                else return false;
+            } 
+            else{
+                if(ten>0 && five>0){
+                    ten--;
+                    five--;
+                }
+                else if(five>=3) five-=3;
+                else return false;
+            }
+        }
+        return true;
+    }
+}
